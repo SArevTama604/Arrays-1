52d114e78428e897c555529f3cd762b7bd971b5a
Salvador <sarevalotamayo@student.rtc.edu>
2024-03-08 21:56:10 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Arrays_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String[] students = new string[5];
+            students[0] = "Salvador";
+            students[1] = "Maria";
+            students[2] = "Ivan";
+            students[3] = "julio";
+            students[4] = "Mariana";
+            
+            foreach (string student in students)
+            {
+                Console.WriteLine(student);
+            }
+            Console.WriteLine(students.Length + " Students");
+        }
+    }
+}
